10a7309048759673aeef77479e13c87dc8ec4d5b
Anthony Yap <anthony.yap@orangeandbronze.com>
2026-08-06 14:54:57 +0800

    feat: add static data, types, and image remote patterns

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export interface Store {
+  id: string;
+  name: string;
+  city: string;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  weight: string;
+  price: number;
+  originalPrice?: number;
+  discountPercent?: number;
+}
+
+export interface StoreProducts {
+  storeId: string;
+  featuredProducts: Product[];
+  priceDrop: Product[];
+}
